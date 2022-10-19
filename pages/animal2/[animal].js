ede1c9b7fc2d7eb75fbfb8096de2c91cdfb418db
Alexander Trefonas <atrefonas@gmail.com>
2022-10-18 21:24:26 -0400

    add serverside props example

--- pages/animal2/[animal].js
+++ pages/animal2/[animal].js
@@ -10,29 +10,18 @@ import CircularProgress from '@mui/material/CircularProgress';
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-export async function getStaticPaths() {
-  return {
-    paths: [
-    { params: { animal: 'dog' } },
-    { params: { animal: 'cat' } }],
-    fallback: true, // can also be true or 'blocking'
-  }
-}
-
-// `getStaticPaths` requires using `getStaticProps`
-export async function getStaticProps(context) {
-  console.log('fetching static props ' + context.params.animal)
+export async function getServerSideProps(context) {
+  console.log('fetching serverside props ' + context.params.animal)
   const animalQuery = context.params.animal;
   const animalTime = await axios.get(`http://localhost:4005/animal/${animalQuery}`)
   .then(res => {
-    console.log('fetched statically!!')
+    console.log('fetched serverside!!')
     return res.data
   }).catch((e) => {console.error(e)})
 
   return {
     // Passed to the page component as props
-    props: { animalTime },
-    revalidate: 1,
+    props: { animalTime }
   }
 }
 
